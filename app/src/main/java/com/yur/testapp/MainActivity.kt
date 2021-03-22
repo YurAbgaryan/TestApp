e64314392395e933258b19d2d43c690716e2e75c
yuri.abgaryan <yur.GitHub95>
2021-03-22 21:11:59 +0400

    removed commented lines in .MainActivity

--- app/src/main/java/com/yur/testapp/MainActivity.kt
+++ app/src/main/java/com/yur/testapp/MainActivity.kt
@@ -42,13 +42,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
 
 
-//        setSupportActionBar(findViewById(R.id.toolbar))
 //
-//
-//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show()
-//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
