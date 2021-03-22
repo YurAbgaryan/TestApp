ba07103ceff8663e2e5df626d98f2f49078f17e9
yuri.abgaryan <yur.GitHub95>
2021-03-22 21:07:53 +0400

    initial commit added 2 files

---
+++ app/src/main/java/com/yur/testapp/MainActivity2.kt
@@ -0,0 +1,14 @@
+package com.yur.testapp
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity2 : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        // Enables Always-on
+    }
+}
